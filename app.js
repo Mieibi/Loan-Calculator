5b14aecc69a4146230f7568789339d35258339c2
miebz <aomieibi@yahoo.com>
2020-05-25 14:39:59 +0100

    Update on css and html

---
+++ app.js
@@ -0,0 +1,16 @@
+//UI Variable
+const form = document.querySelector("#loan-form");
+
+//add Listener
+form.addEventListener('submit', calculate);
+
+//Calculate Results
+function calculate(e){
+    console.log("boy");
+
+
+
+    e.preventDefault();
+}
+
+
